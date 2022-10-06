5e901587812ffe0a91d0c97d240e7657cb0db8bc
유ìkimdaehwan <kdh4642@naver.com>
2022-10-06 11:39:05 +0900

    모르겠다

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className='container'>
+        <input 
+        type="text"
+        name="title"
+        // onChange={onChangeHandler} 
+        />
+        <button className='add-button'>추가하기</button>
+        <div className='Haeder'> Todo List</div>
+      </div>
+
+      <div>
+        
+      </div>
+
+      <div>
+
+      </div>
+    </main>
   );
 }
 
